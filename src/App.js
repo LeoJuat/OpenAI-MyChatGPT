ecda07dae20e7bc5cd0e41d1538529fcab8b2cda
leojuat <leonellejuat@gmail.com>
2023-03-21 22:55:51 -0700

    added user and bot messages

--- src/App.js
+++ src/App.js
@@ -1,14 +1,46 @@
 // import mySvg from "./assets/send.svg";
-// import bot from "./assets/bot.svg";
-// import user from "./assets/user.svg";
+import bot from "./assets/bot.svg";
+import user from "./assets/user.svg";
 
 function App() {
   return (
     <>
       <div className="App">
         <aside className="sidebar">
-          <h1>Aside</h1>
+          <div className="side-menu-button">
+            <span className="side-menu-btn-span">+</span>New Chat
+          </div>
         </aside>
+        <section className="chatbox">
+          <div className="chat-log">
+            <div className="chat-message">
+              <div className="chat-message-center">
+                <div className="avatar">
+                  <img className="avatar-img" src={user} alt="user avatar" />
+                </div>
+                <div className="message">Hello world</div>
+              </div>
+            </div>
+          </div>
+
+          <div className="chat-message bot">
+            <div className="chat-message-center">
+              <div className="avatar bot">
+                <img className="avatar-img bot" src={bot} alt="bot avatar" />
+              </div>
+              <div className="message">I am an AI</div>
+            </div>
+          </div>
+
+          <div className="chat-input-holder">
+            <textarea
+              className="textarea"
+              rows="1"
+              placeholder="Ask Codex..."
+              style={{ resize: "none" }}
+            ></textarea>
+          </div>
+        </section>
       </div>
     </>
   );
